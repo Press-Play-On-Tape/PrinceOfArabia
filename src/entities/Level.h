8d1f9e7c78638e634191456f5d4d1916706b655f
Mr.Blinky <mstr.blinky@gmail.com>
2023-01-03 20:32:08 +0100

    update Level.h
    fixed incorrect offset value in FX::seekDataArray when xLoc < 3 in loadMap
    optimized LoadMap a bit saving ~8 bytes

--- src/entities/Level.h
+++ src/entities/Level.h
@@ -1041,26 +1041,30 @@ struct Level {
         void loadMap(GamePlay &gamePlay) {
 
 
-            // Background ..
-
-            for (uint8_t x = 0; x < 16; x++) {
-
-                bg[0][x] = TILE_NONE;
+            uint8_t offset = 0;
+            if (this->xLoc) {
+                offset = this->xLoc - 3;
             }
 
-            for (int8_t y = this->yLoc - 1; y < yLoc + 4; y++) {
+            // Background ..
+
+            for (uint8_t x = 0; x < 5 * 16; x++) {
+
+                *(&bg[0][0] + x) = TILE_NONE;
+            }
+
+            for (int8_t y = this->yLoc - 1; y < (int8_t)(this->yLoc + 4); y++) {
 
                 if (y >= 0) {
+                    FX::seekDataArray(FX::readIndexedUInt24(Levels::Level_BG, gamePlay.level), y, offset, this->width);
 
-                    FX::seekDataArray(FX::readIndexedUInt24(Levels::Level_BG, gamePlay.level), y, this->xLoc -3, this->width);
+                    for (uint8_t x = 0; x < 16; x++) {
 
-                        for (uint8_t x = 0; x < 16; x++) {
-
-                            int8_t tileId = static_cast<int8_t>(FX::readPendingUInt8());
-                            if (x < 3 && this->xLoc == 0) tileId = TILE_NONE;
-                            bg[y - this->yLoc + 1][x] = tileId;
-
+                        if (x >= 3 || offset) {
+                            bg[y - this->yLoc + 1][x] = static_cast<int8_t>(FX::readPendingUInt8());
                         }
+
+                    }
 
                     FX::readEnd();
                 }
@@ -1070,24 +1074,23 @@ struct Level {
 
             // Foreground ..
 
-                for (uint8_t x = 0; x < 16; x++) {
+                for (uint8_t x = 0; x < 5 * 16; x++) {
 
-                    fg[0][x] = TILE_FG_WALL_1; // y - this->yLoc + 1 always evaluates to 0 here
+                    *(&fg[0][0] + x) = TILE_FG_WALL_1;
 
                 }
 
-            for (int8_t y = this->yLoc - 1; y < this->yLoc + 4; y++) {
+            for (int8_t y = this->yLoc - 1; y < (int8_t)(this->yLoc + 4); y++) {
 
                 if (y >= 0) {
 
-                    FX::seekDataArray(FX::readIndexedUInt24(Levels::level_FG, gamePlay.level), y, this->xLoc - 3, this->width);
+                    FX::seekDataArray(FX::readIndexedUInt24(Levels::level_FG, gamePlay.level), y, offset, this->width);
 
                     for (uint8_t x = 0; x < 16; x++) {
 
-                        int8_t tileId = static_cast<int8_t>(FX::readPendingUInt8());
-                        if (x < 3 && this->xLoc == 0) tileId = TILE_FG_WALL_1;
-                        fg[y - this->yLoc + 1][x] = tileId;
-
+                        if (x >= 3 || offset) {
+                            fg[y - this->yLoc + 1][x] = static_cast<int8_t>(FX::readPendingUInt8());
+                        }
                     }
 
                     FX::readEnd();
