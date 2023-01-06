c6520cffaf53587976cbc8d5149fd64502dee251
Filmote <simon@bloggingadeadhorse.com>
2023-01-06 21:42:25 +1100

    Save Work

--- PrinceOfPersia_Game.ino
+++ PrinceOfPersia_Game.ino
@@ -11,7 +11,7 @@
 
 void game_Init() {
 
-    gamePlay.init(arduboy, 7);
+    gamePlay.init(arduboy, 1);
 
     #ifndef SAVE_MEMORY_ENEMY
         level.init_PositionChars(gamePlay, prince, enemy, true);
@@ -561,6 +561,7 @@ void game() {
                                 DEBUG_PRINTLN(F("DOWN_BUTTON, Climb down Pos 2"));
                                 #endif
                                 prince.pushSequence(Stance::Step_Climbing_15_End, Stance::Step_Climbing_1_Start, Stance::Jump_Up_A_14_End, true);
+                                prince.setHangingCounter(90);
                                 break;
 
                             case CanClimbDownResult::StepThenClimbDown:
@@ -570,6 +571,7 @@ void game() {
 
                                 prince.pushSequence(Stance::Step_Climbing_15_End, Stance::Step_Climbing_1_Start, Stance::Jump_Up_A_14_End, true);
                                 prince.pushSequence(Stance::Small_Step_6_End, Stance::Small_Step_1_Start, Stance::Upright, false);
+                                prince.setHangingCounter(120);
                                 break;
 
                             case CanClimbDownResult::TurnThenClimbDown:
@@ -579,6 +581,7 @@ void game() {
 
                                 prince.pushSequence(Stance::Step_Climbing_15_End, Stance::Step_Climbing_1_Start, Stance::Jump_Up_A_14_End, true);
                                 prince.pushSequence(Stance::Standing_Turn_1_Start, Stance::Standing_Turn_5_End, Stance::Upright_Turn, false);
+                                prince.setHangingCounter(120);
                                 break;
 
                             case CanClimbDownResult::StepThenTurnThenClimbDown:
@@ -589,6 +592,7 @@ void game() {
                                 prince.pushSequence(Stance::Step_Climbing_15_End, Stance::Step_Climbing_1_Start, Stance::Jump_Up_A_14_End, true);
                                 prince.pushSequence(Stance::Standing_Turn_1_Start, Stance::Standing_Turn_5_End, Stance::Upright_Turn, false);
                                 prince.pushSequence(Stance::Small_Step_1_Start, Stance::Small_Step_6_End, Stance::Upright, false);
+                                prince.setHangingCounter(150);
                                 break;
 
                             default:
@@ -742,7 +746,7 @@ void game() {
 
                     // Drop after a period of time hanging ..
 
-                    else if(prince.getHangingCounter() == 0) {
+                    else if (prince.getHangingCounter() == 0) {
 
                         CanClimbDownPart2Result climbDownResult = level.canClimbDown_Part2(prince, 0);
 
