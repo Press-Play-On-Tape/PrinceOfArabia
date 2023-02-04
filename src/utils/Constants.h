b761c41417c022fca38fb257d9ada3d71d675e5c
Filmote <simon@bloggingadeadhorse.com>
2023-02-04 16:12:35 +1100

    Update Constants.h

--- src/utils/Constants.h
+++ src/utils/Constants.h
@@ -16,17 +16,17 @@
     #define LEVEL_DATA_FROM_FX
     #define IMAGE_DATA_FROM_FX
     #define _SAVE_TO_FX
-    #define STARTING_LEVEL 6
+    #define STARTING_LEVEL 1
     #define _DEBUG_CUT_SCENES
 
     #define _DEBUG
 
 #else
 
-    #define _SAVE_MEMORY_USB
+    #define SAVE_MEMORY_USB
     #define _SAVE_MEMORY_OTHER
-    #define SAVE_MEMORY_SOUND
-    #define SAVE_MEMORY_ENEMY
+    #define _SAVE_MEMORY_SOUND
+    #define _SAVE_MEMORY_ENEMY
     #define _SAVE_MEMORY_TITLE
     #define _ALT_B_BUTTON
     #define _GIVE_SWORD
@@ -34,8 +34,8 @@
     #define LEVEL_DATA_FROM_FX
     #define IMAGE_DATA_FROM_FX
     #define _SAVE_TO_FX
-    #define STARTING_LEVEL 6
-    #define DEBUG_CUT_SCENES
+    #define STARTING_LEVEL 7
+    #define _DEBUG_CUT_SCENES
 
     #define _DEBUG
 
