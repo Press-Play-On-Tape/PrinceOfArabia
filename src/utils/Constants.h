eaea403da48e0c773059bdc2360e86465c5ea909
Filmote <simon@bloggingadeadhorse.com>
2023-03-18 12:50:44 +1100

    Save Work

--- src/utils/Constants.h
+++ src/utils/Constants.h
@@ -16,7 +16,7 @@
     #define LEVEL_DATA_FROM_FX
     #define MOVEMENT_DATA_FROM_FX
     #define _SAVE_TO_FX
-    #define STARTING_LEVEL 2
+    #define STARTING_LEVEL 1
     #define MAX_LEVEL 13
     #define _DEBUG_CUT_SCENES
     #define _DEBUG_LEVELS
