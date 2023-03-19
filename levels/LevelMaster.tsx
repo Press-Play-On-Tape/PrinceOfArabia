f9e52d7a0355742237e19cf06e9cb667510c173e
Simon Holmes <simon@bloggingadeadhorse.com>
2023-03-19 14:46:15 +1100

    Save Work (#127)

--- levels/LevelMaster.tsx
+++ levels/LevelMaster.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.9" tiledversion="1.9.2" name="tile_dungeon" tilewidth="21" tileheight="31" tilecount="57" columns="0">
+<tileset version="1.9" tiledversion="1.9.2" name="tile_dungeon" tilewidth="21" tileheight="31" tilecount="96" columns="0">
  <grid orientation="orthogonal" width="1" height="1"/>
  <tile id="86">
   <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_12.png"/>
@@ -152,24 +152,141 @@
   <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_43.png"/>
  </tile>
  <tile id="124">
-  <image width="12" height="31" source="../images/tiles/Backuptile_dungeon_50_00.png"/>
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_99_00.png"/>
  </tile>
  <tile id="29">
-  <image width="21" height="31" source="../images/tiles/BackupCollapsedTile.png"/>
+  <image width="21" height="31" source="../images/tiles/CollapsedTile.png"/>
  </tile>
  <tile id="30">
-  <image width="12" height="31" source="../images/tiles/BackupCollapsedTile_Half.png"/>
+  <image width="12" height="31" source="../images/tiles/CollapsedTile_Half.png"/>
  </tile>
  <tile id="31">
-  <image width="12" height="31" source="../images/tiles/BackupColumn_BL.png"/>
+  <image width="12" height="31" source="../images/tiles/Column_BL.png"/>
  </tile>
  <tile id="32">
-  <image width="12" height="31" source="../images/tiles/BackupColumn_BR.png"/>
+  <image width="12" height="31" source="../images/tiles/Column_BR.png"/>
  </tile>
  <tile id="33">
-  <image width="12" height="31" source="../images/tiles/BackupColumn_TL.png"/>
+  <image width="12" height="31" source="../images/tiles/Column_TL.png"/>
  </tile>
  <tile id="34">
-  <image width="12" height="31" source="../images/tiles/BackupColumn_TR.png"/>
+  <image width="12" height="31" source="../images/tiles/Column_TR.png"/>
+ </tile>
+ <tile id="35">
+  <image width="12" height="31" source="../images/tiles/Column_TF.png"/>
+ </tile>
+ <tile id="36">
+  <image width="12" height="31" source="../images/tiles/Column_BF.png"/>
+ </tile>
+ <tile id="37">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_56.png"/>
+ </tile>
+ <tile id="38">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_57.png"/>
+ </tile>
+ <tile id="39">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_58.png"/>
+ </tile>
+ <tile id="40">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_59.png"/>
+ </tile>
+ <tile id="41">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_60.png"/>
+ </tile>
+ <tile id="42">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_61.png"/>
+ </tile>
+ <tile id="43">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_62.png"/>
+ </tile>
+ <tile id="44">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_63.png"/>
+ </tile>
+ <tile id="45">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_64.png"/>
+ </tile>
+ <tile id="46">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_65.png"/>
+ </tile>
+ <tile id="47">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_66.png"/>
+ </tile>
+ <tile id="48">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_67.png"/>
+ </tile>
+ <tile id="49">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_68.png"/>
+ </tile>
+ <tile id="50">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_69.png"/>
+ </tile>
+ <tile id="51">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_70.png"/>
+ </tile>
+ <tile id="52">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_71.png"/>
+ </tile>
+ <tile id="53">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_72.png"/>
+ </tile>
+ <tile id="54">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_73.png"/>
+ </tile>
+ <tile id="55">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_74.png"/>
+ </tile>
+ <tile id="56">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_75.png"/>
+ </tile>
+ <tile id="57">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_76.png"/>
+ </tile>
+ <tile id="58">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_77.png"/>
+ </tile>
+ <tile id="59">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_78.png"/>
+ </tile>
+ <tile id="60">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_79.png"/>
+ </tile>
+ <tile id="61">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_80.png"/>
+ </tile>
+ <tile id="62">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_81.png"/>
+ </tile>
+ <tile id="63">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_82.png"/>
+ </tile>
+ <tile id="64">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_83.png"/>
+ </tile>
+ <tile id="65">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_84.png"/>
+ </tile>
+ <tile id="66">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_85.png"/>
+ </tile>
+ <tile id="67">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_86.png"/>
+ </tile>
+ <tile id="68">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_87.png"/>
+ </tile>
+ <tile id="69">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_88.png"/>
+ </tile>
+ <tile id="70">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_89.png"/>
+ </tile>
+ <tile id="71">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_90.png"/>
+ </tile>
+ <tile id="72">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_91.png"/>
+ </tile>
+ <tile id="73">
+  <image width="12" height="31" source="../images/tiles/Backup/tile_dungeon_92.png"/>
  </tile>
 </tileset>
