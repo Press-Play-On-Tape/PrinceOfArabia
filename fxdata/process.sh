2a9fe82d0902ffed76118523abefa1ed22b959bc
Filmote <simon@bloggingadeadhorse.com>
2023-03-26 14:48:50 +1100

    Save Work

--- fxdata/process.sh
+++ fxdata/process.sh
@@ -40,10 +40,10 @@ montage ../images/prince/left/Backup/???_*.png -geometry 36x36+0+0 -background n
 montage ../images/prince/right/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 15x15 ../images/prince/right/Prince_Right_36x36.png
 montage ../images/mirror/left/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 15x15 ../images/mirror/left/Mirror_Left_36x36.png
 montage ../images/mirror/right/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 15x15 ../images/mirror/right/Mirror_Right_36x36.png
-montage ../images/enemy/left/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 15x15 ../images/enemy/left/Enemy_Left_36x36.png
-montage ../images/enemy/right/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 15x15 ../images/enemy/right/Enemy_Right_36x36.png
-montage ../images/skeleton/left/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 15x15 ../images/skeleton/left/Skeleton_Left_36x36.png
-montage ../images/skeleton/right/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 15x15 ../images/skeleton/right/Skeleton_Right_36x36.png
+montage ../images/enemy/left/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 10x4 ../images/enemy/left/Enemy_Left_36x36.png
+montage ../images/enemy/right/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 10x4 ../images/enemy/right/Enemy_Right_36x36.png
+montage ../images/skeleton/left/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 10x4 ../images/skeleton/left/Skeleton_Left_36x36.png
+montage ../images/skeleton/right/Backup/???_*.png -geometry 36x36+0+0 -background none -tile 10x4 ../images/skeleton/right/Skeleton_Right_36x36.png
 
 python3 scripts/createMovements.py ../src/utils/Constants.h movements.txt
 python3 scripts/createSEQData.py ../src/utils/Constants.h ../src/utils/movements_SEQData.h
