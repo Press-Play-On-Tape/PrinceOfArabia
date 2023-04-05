dc469c649a83f0f481225396b3075206f88a8094
Filmote <simon@bloggingadeadhorse.com>
2023-04-05 17:41:42 +1000

    Save Work

--- fxdata/scripts/img2sheet.py
+++ fxdata/scripts/img2sheet.py
@@ -2,16 +2,21 @@
 
 import sys
 import os
-from PIL import Image
+from PIL import Image, ImageOps
 from math import sqrt
 
 spacing = 0
 
-if (len(sys.argv) < 2) or (len(sys.argv) > 3):
-  sys.stderr.write("Usage: {} imagefolder [outputfile]\nWhen no outputfile is given, the imagesheet will be created in the parent folder.".format(os.path.basename(sys.argv[0])))
+if (len(sys.argv) < 2) or (len(sys.argv) > 4):
+  sys.stderr.write("Usage: {} imagefolder [outputfileBase] [flip=False]\nWhen no outputfile is given, the imagesheet will be created in the parent folder.".format(os.path.basename(sys.argv[0])))
   sys.exit(-1)
 
 imagepath = sys.argv[1]
+flip = False
+
+if len(sys.argv) == 4:
+  if sys.argv[3].upper() == "TRUE":
+      flip = True
 if os.path.isfile(imagepath):
   imagepath = os.path.dirname(os.path.abspath(imagepath))
 imagepath += os.sep  
@@ -36,13 +41,16 @@ c = 0
 r = 0
 for i in range(len(images)):
   img = Image.open(imagepath + images[i]).convert("RGBA")
+  if flip:
+    img = ImageOps.mirror(img)
   new.paste(img,(c * (width+spacing) + spacing, r * (height + spacing) + spacing))
   c += 1
   if c == cols:
     c = 0
     r += 1
-if len(sys.argv) == 3:
-  filename =sys.arg[2]
+if len(sys.argv) >= 3:
+  filename =sys.argv[2]
+  filename = '{}_{}x{}_{}.png'.format(filename,width,height,spacing)
 elif spacing > 0:
   filename = '{}{}{}_{}x{}_{}.png'.format(os.path.dirname(imagepath[:-1]), os.sep, os.path.basename(imagepath[:-1]),width,height,spacing)
 else:  
